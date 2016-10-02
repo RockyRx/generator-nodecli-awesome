a5ed5cdc55323b7d5deba222b5a196caa77bbf8c
Sebastian Schürmann <sebs@2xs.org>
2016-10-03 00:38:59 +0200

    Refactored the build

--- app/index.js
+++ app/index.js
@@ -1,113 +1,100 @@
-'use strict'
-var util = require('util')
-var path = require('path')
-var yeoman = require('yeoman-generator')
-var gitconfig = require('git-config')
+var generators = require('yeoman-generator');
+var util = require('util');
+var path = require('path');
+var gitconfig = require('git-config');
+var slugify = require("underscore.string/slugify");
+var camelize = require("underscore.string/camelize");
 
-var NodejsGenerator = module.exports = function NodejsGenerator (args, options, config) {
-  yeoman.generators.Base.apply(this, arguments)
 
-  this.on('end', function () {
-    this.installDependencies({ skipInstall: options['skip-install']})
-  })
-
-  this.pkg = JSON.parse(this.readFileAsString(path.join(__dirname, '../package.json')))
-}
-
-util.inherits(NodejsGenerator, yeoman.generators.Base)
-
-NodejsGenerator.prototype.askFor = function askFor () {
-  var cb = this.async()
-
-  // have Yeoman greet the user.
-  console.log(this.yeoman)
-
-  var config = gitconfig.sync()
-
-  var prompts = [
-    {
-      type: 'input',
-      name: 'moduleName',
-      message: 'node.js module name:',
-      default: path.basename(process.cwd())
-    },
-    {
-      type: 'input',
-      name: 'moduleDesc',
-      message: 'Module description'
-    },
-    {
-      type: 'input',
-      name: 'keywords',
-      message: 'Module keywords',
-      filter: function (value) {
-        if (typeof value === 'string') {
-          value = value.split(',')
+module.exports = generators.Base.extend({
+  build: function () {
+    console.log('build');
+    this.mkdir('man')
+    this.template('man.md', './man/'+this.cliName+'.md')
+    this.template('_package.json', 'package.json')
+    this.copy('travis.yml', '.travis.yml')
+    this.copy('gitignore', '.gitignore')
+    this.copy('LICENSE', 'LICENSE')
+    this.copy('CHANGELOG.md', 'CHANGELOG.md')
+    this.template('README.md', 'README.md')
+    this.mkdir('bin')
+    this.template('cli', ('bin/cli'))
+  },
+  testFrameworks: function () {
+    console.log('test');
+    this.mkdir('test')
+    this.mkdir('test/fixtures')
+    this.copy('lib.js', 'index.js')
+    this.template('test.js', 'test/index.js')
+  },
+  prompting: function () {
+    var config = gitconfig.sync();
+    var prompts = [
+      {
+        type: 'input',
+        name: 'moduleName',
+        message: 'node.js module name:',
+        default: path.basename(process.cwd())
+      },
+        {
+          type: 'input',
+          name: 'moduleDesc',
+          message: 'Module description'
+        },
+        {
+          type: 'input',
+          name: 'keywords',
+          message: 'Module keywords',
+          filter: function (value) {
+            if (typeof value === 'string') {
+              value = value.split(',')
+            }
+            return value
+              .map(function (val) {
+                return val.trim()
+              })
+              .filter(function (val) {
+                return val.length > 0
+              })
+          }
+        },
+        {
+          type: 'input',
+          name: 'githubName',
+          message: 'Your github username',
+          default: (config.github && config.github.user) || ''
+        },
+        {
+          type: 'input',
+          name: 'cliName',
+          message: 'Cli Name',
+          default: config.moduleName
+        },
+        {
+          type: 'input',
+          name: 'author',
+          message: 'Author name',
+          default: ((config.user && config.user.name) || '') +
+            (' <' + ((config.user && config.user.email) || '') + '>')
         }
-        return value
-          .map(function (val) {
-            return val.trim()
-          })
-          .filter(function (val) {
-            return val.length > 0
-          })
+      ];
+    return this.prompt(prompts).then(function (props) {
+
+      this.moduleName = slugify(props.moduleName)
+      this.moduleVarName = camelize(props.moduleName)
+      this.moduleDesc = props.moduleDesc
+      this.keywords = props.keywords
+      this.githubName = props.githubName
+      this.author = props.author
+      this.cliName = props.cliName
+      this.copyrightName = props.author.replace(/<[^>]*?>/gm, '').trim()
+
+      this.dequote = function (str) {
+        return str.replace(/\"/gm, '\\"')
       }
-    },
-    {
-      type: 'input',
-      name: 'githubName',
-      message: 'Your github username',
-      default: (config.github && config.github.user) || ''
-    },
-    {
-      type: 'input',
-      name: 'cliName',
-      message: 'Cli Name',
-      default: config.moduleName
-    },
-    {
-      type: 'input',
-      name: 'author',
-      message: 'Author name',
-      default: ((config.user && config.user.name) || '') +
-        (' <' + ((config.user && config.user.email) || '') + '>')
-    }
-  ]
 
-  this.prompt(prompts, function (props) {
-    this.moduleName = this._.slugify(props.moduleName)
-    this.moduleVarName = this._.camelize(props.moduleName)
-    this.moduleDesc = props.moduleDesc
-    this.keywords = props.keywords
-    this.githubName = props.githubName
-    this.author = props.author
-    this.cliName = props.cliName
-    this.copyrightName = props.author.replace(/<[^>]*?>/gm, '').trim()
+      this.log('app name', props, this);
 
-    this.dequote = function (str) {
-      return str.replace(/\"/gm, '\\"')
-    }
-
-    cb()
-  }.bind(this))
-}
-
-NodejsGenerator.prototype.build = function build () {
-  this.mkdir('man')
-  this.template('man.md', './man/'+this.cliName+'.md')
-  this.template('_package.json', 'package.json')
-  this.copy('travis.yml', '.travis.yml')
-  this.copy('gitignore', '.gitignore')
-  this.copy('LICENSE', 'LICENSE')
-  this.copy('CHANGELOG.md', 'CHANGELOG.md')
-  this.template('README.md', 'README.md')
-  this.mkdir('bin')
-  this.template('cli', ('bin/cli'))
+    }.bind(this));
   }
-
-NodejsGenerator.prototype.testFrameworks = function mocha () {
-  this.mkdir('test')
-  this.mkdir('test/fixtures')
-  this.copy('lib.js', 'index.js')
-  this.template('test.js', 'test/index.js')
-}
+});
