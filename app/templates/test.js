4038a521bddc687d0a4c277bb2dac0012bad7d7b
Sebastian Schürmann <sebs@2xs.org>
2016-10-03 01:09:08 +0200

    improved test

--- app/templates/test.js
+++ app/templates/test.js
@@ -1,9 +1,35 @@
-var expect = require('chai').expect
-var <%= moduleVarName %> = require('..')
+var expect = require('chai').assert
+var cli = require('..')();
 
 describe('<%= moduleName %>', function() {
-  it('should say hello', function(done) {
-    expect(<%= moduleVarName %>()).to.equal('Hello, world');
+  it('have a parse method', function(done) {
+    assert.ok(cli.parse);
     done();
   });
+
+  'use strict';
+  const assert = require('chai').assert;
+  var exec = require('child-process-promise').exec;
+
+  var commands =[
+    './bin/cli -h',
+    './bin/cli a',
+    'echo foo | ./bin/cli'
+  ];
+
+  it('executes the commands', function(done) {
+
+    function execAndDone(command){
+      return exec(command).catch(done);
+    }
+
+    commands = commands.map(execAndDone);
+    Promise
+      .all(commands)
+      .then(function (result) {
+          result.map(assert.ok);
+          done();
+      }).catch(done);
+  }).timeout(5000);
+
 });
